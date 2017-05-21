cd93e46a65c96f46d5a8d7c7de01b79de5becbb5
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-05-21 23:33:40 +0300

    fixed groovy script

--- jobs/hello-world.groovy
+++ jobs/hello-world.groovy
@@ -1,1 +1,11 @@
-def gitrepo = 'https://github.com/osemych/hello-world.git'job('job-hello-world') {    scm {        git(gitrepo)    }    steps {        shell('echo Hello world!')    }}
+def gitrepo = 'https://github.com/osemych/hello-world.git'
+
+job('job-hello-world') {
+  scm {
+    git(gitrepo)
+  }
+  steps {
+    shell('echo Hello world!')
+  }
+}
+
