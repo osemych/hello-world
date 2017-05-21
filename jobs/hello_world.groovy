950ff96a83d7fb957e5da6d73094097acc6aa930
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-05-22 00:01:37 +0300

    fixed script filename

---
+++ jobs/hello_world.groovy
@@ -0,0 +1,13 @@
+String gitrepo = 'https://github.com/osemych/hello-world.git'
+
+job("job-hello-world") {
+  scm {
+    git gitrepo
+  }
+  trigers {
+    scm 'H/5 * * * *'
+  }
+  steps {
+    shell 'sh Hello world!'
+  }
+}
