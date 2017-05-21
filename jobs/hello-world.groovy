37e90ccf1e0be8db47421e5cecfc1e8271c599fa
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-05-21 23:24:39 +0300

    fixed groovy script

--- jobs/hello-world.groovy
+++ jobs/hello-world.groovy
@@ -1,10 +1,10 @@
-def giturl = 'https://github.com/osemych/hello-world.git'
+def giturl = "https://github.com/osemych/hello-world.git"
 
 job("job-hello-world") {
   scm {
     git(giturl)
   }
   steps {
-     shell('sh hello-world.sh')
+     shell("sh hello-world.sh")
   }
 }
