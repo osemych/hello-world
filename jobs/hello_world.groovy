044604734cc706dad7bdb97032f356aa903c7bfa
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-05-28 17:54:34 +0300

    updated dsl job

--- jobs/hello_world.groovy
+++ jobs/hello_world.groovy
@@ -1,13 +1,22 @@
 String gitrepo = 'https://github.com/osemych/hello-world.git'
 
-job("job-hello-world") {
-  scm {
-    git gitrepo
-  }
-  triggers {
-    scm 'H/5 * * * *'
-  }
-  steps {
-    shell 'sh hello-world.sh'
-  }
+job("job-hello-world-slave01") {
+    label(slave-node01)
+    scm {
+        git gitrepo
+    }
+    steps {
+        shell 'sh hello-world.sh'
+    }
 }
+
+job("job-hello-world-slave02") {
+    label(slave-node02)
+    scm {
+        git gitrepo
+    }
+    steps {
+        shell 'sh hello-world.sh'
+    }
+}
+
