f20493fa2de6d359be5226572846fb1c9ff514c0
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-05-28 21:46:57 +0300

    fixed syntax error in job loop

--- jobs/hello_world.groovy
+++ jobs/hello_world.groovy
@@ -2,11 +2,12 @@ String gitrepo = 'https://github.com/osemych/hello-world.git'
 
 while (index <= 2) {
     job('job-hello-world-slave0'+index) {
-    label('slave-node0'+index)
-    scm {
-        git gitrepo
-    }
-    steps {
-        shell 'sh hello-world.sh'
+        label('slave-node0'+index)
+        scm {
+            git gitrepo
+        }
+        steps {
+            shell 'sh hello-world.sh'
+        }
     }
 }
